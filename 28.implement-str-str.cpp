b90991c7749a5beeda0dead8f926c9c9bb3589a7
Eric He <codeforce2200@gmail.com>
2019-09-09 02:01:41 -0400

    init

---
+++ 28.implement-str-str.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=28 lang=cpp
+ *
+ * [28] Implement strStr()
+ */
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        if(m == 0)
+            return 0;
+        for(int i = 0; i+m-1 < n; ++i) {
+            if(haystack.substr(i, m) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
